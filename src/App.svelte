6e4429b54ddc3b0eb2f935c9d8e623e3a3e60244
auleki <kodagiwa@gmail.com>
2020-12-31 07:25:32 +0100

    adding two way binding

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
 	let name = "Ade";
 	let car = "Benz";
+	let color = "red";
+	let bgColor = "indigo"
 	const changeCar = (carBrand) => {
 		car = carBrand;
 	}
+
+	//ANCHOR comments will come in useful
 
 	const takeInput = ({ target }) => {
 		car = target.value
@@ -12,18 +16,38 @@
 
 <main>
 	<h1>{car}</h1>
+	<h4 style="color: {color}">
+		{color}
+	</h4>
 	<button on:click={() => changeCar("toyota")}>
 		change car
 	</button>
-	<input type="text" on:input={takeInput} value={car}>
+	<div class="box" style="background-color: {bgColor};"></div>
+	<input type="text" bind:value={car} >
+	<input type="text" placeholder="color" bind:value={color}>
+	<input type="text" placeholder="color" bind:value={bgColor}>
+
+	<!-- <input type="text" on:input={takeInput} value={car}> -->
+
 </main>
 
 <style>
 	main {
 		text-align: center;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		justify-content: space-evenly;
+		align-items: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+
+	.box {
+		height: 10em;
+		width: 10em;
+		
 	}
 
 	h1 {
@@ -32,6 +56,13 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+
+	h4 {
+		font-size: 2em;
+		font-weight: 100;
+		text-transform: uppercase;
+	}
+
 
 	@media (min-width: 640px) {
 		main {
