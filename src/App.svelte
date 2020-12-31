a7b302fff697eaca5d998c571090c885a746b2f0
auleki <kodagiwa@gmail.com>
2020-12-31 07:07:13 +0100

    setting up disvelte

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	let name = "Ade";
+	let car = "Benz";
+	const changeCar = (carBrand) => {
+		car = carBrand;
+	}
+
+	const takeInput = ({ target }) => {
+		car = target.value
+	}
+</script>
+
+<main>
+	<h1>{car}</h1>
+	<button on:click={() => changeCar("toyota")}>
+		change car
+	</button>
+	<input type="text" on:input={takeInput} value={car}>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		/* text-transform: uppercase; */
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
